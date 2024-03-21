67df27d3bf01f22346c662a66e50ddcf8808b95c
FacuMieres13 <aguscora00@gmail.com>
2024-03-21 14:36:17 -0300

    Creating products section and applying styles

--- src/components/Nosotros.js
+++ src/components/Nosotros.js
@@ -9,6 +9,7 @@ export const Nosotros = () => {
                 <div className='nosotros-history'>
                     <h1 className='nosotros-title'>NOSOTROS</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vehicula nisi ac justo tincidunt, quis ultrices sapien pulvinar. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras sit amet risus eget ligula facilisis auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce vehicula tristique turpis, sed bibendum lacus luctus ac. </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vehicula nisi ac justo tincidunt, quis ultrices sapien pulvinar. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras sit amet risus eget ligula facilisis auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce vehicula tristique turpis, sed bibendum lacus luctus ac. </p>
                 </div>
             </div>
         </div>
--- src/components/Trabajos.js
+++ src/components/Trabajos.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import '../App.css';
+import postre from './media/postre.png';
 export const Trabajos = () => {
   return (
     <div className='section-trabajos'>
-        <figure>
-            test
+        <h2 className='title-trabajos'>TRABAJOS</h2>
+        <figure className='trabajos-imgs'>
+        <img src={postre} />
+        <img src={postre} />
+        <img src={postre} />
+        <img src={postre} />
+        <img src={postre} />
+        <img src={postre} />
+        <img src={postre} />
+        <img src={postre} />
+        <img src={postre} />
         </figure>
     </div>
   )
